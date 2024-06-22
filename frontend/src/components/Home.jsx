5204e75b78ab0ccf828966923e92a761574d1cdf
Bugbuster2004 <siddhant6338bansal@gmail.com>
2024-06-23 03:12:15 +0530

    home page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import coffeeImage from "../assets/images/coffee-8595772_1280.jpg"; // Import image as a static asset
+import book from "../assets/images/book1.png"; // Import book image
+
+function Home() {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row h-screen">
+        {/* Bookstore section */}
+        <div className="w-full md:w-1/2 bg-blue-black-gradient text-center flex flex-col items-center justify-center h-1/2 md:h-screen">
+          <h1 className="text-6xl font-bold text-white mt-10">Welcome to the store</h1>
+          <button
+            type="button"
+            className="mt-10 py-2.5 px-5 mb-2 text-sm font-medium text-white focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            View Books
+          </button>
+        </div>
+        {/* Image section */}
+        <div className="w-full md:w-1/2 text-center flex items-center justify-center h-1/2 md:h-screen bg-blue-black-gradient">
+          <img
+          style={{height:"20rem",width:"20rem"}}
+            src={book}
+            alt="Book Image"
+            className="object-cover h-full w-full md:h-auto md:w-auto rounded-full"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens:{
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
+      'xs':{min:'0px', max: '639px' },
+      'xxs':{max:'580px'}
+    },
+
+    extend: {
+      backgroundImage: {
+        'blue-black-gradient': 'linear-gradient(to bottom, #0d1b2a, #1f3550)'
+      },
+    },
+  },
+  plugins: [],
+}
+
