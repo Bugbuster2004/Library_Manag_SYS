3ffe377f6ffdb0cfd390bf7def1e7dde69da103a
Bugbuster2004 <siddhant6338bansal@gmail.com>
2024-07-05 15:45:40 +0530

    adjusted some margin padding issues and logo issues

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -26,9 +26,6 @@ const navigate  = useNavigate()
             alert("Registered Successfully!");
             navigate("/login")
     
-            // Security Consideration:
-            // - Implement proper authentication logic (e.g., JWT)
-            // - Avoid storing passwords on the frontend
     
           } else {
             alert( "Registration failed.");
@@ -47,8 +44,7 @@ const navigate  = useNavigate()
         <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-          Flowbite    
+            
       </a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -58,7 +54,7 @@ const navigate  = useNavigate()
               </h1>
               <form class="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
               <div>
-                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
+                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                       <input type="text" onChange={(e)=>{setname(e.target.value)}} name="name" id="name" placeholder="Enter you name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   <div>
@@ -66,7 +62,7 @@ const navigate  = useNavigate()
                       <input type="email" onChange={(e)=>{setemail(e.target.value)}} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                       <input type="password" onChange={(e)=>{setpassword(e.target.value)}} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://i.pinimg.com/originals/cc/b9/3c/ccb93cb3ea1ccf8b012c1c3a3fa52bf0.jpg" className="h-10 rounded-lg dark:bg-gray-900" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BOOK STORE</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BookWise </span>
                     </a>
                     <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={isMenuOpen ? "true" : "false"}>
                         <span className="sr-only">Open main menu</span>
